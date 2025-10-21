688853fe84dbdd2366a195026ec0fc5d78a40a8e
Sohair Mahmoud <sohair772011@gmail.com>
2025-10-21 21:09:17 +0300

    edit js

--- index.js
+++ index.js
@@ -111,10 +111,3 @@ if (path.includes("home.html")) {
   });
 }
 
-
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/sohairmahmoud77/smart-login.git
-git push -u origin main
